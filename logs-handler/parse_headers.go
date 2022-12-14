58f3835a46677e8bf3e4e3ed78fa68838a254afa
theteacat <joshua@firetail.io>
2022-12-14 10:05:46 +0000

    Fix parseMultivalueHeaders

--- logs-handler/parse_headers.go
+++ logs-handler/parse_headers.go
@@ -41,19 +41,21 @@ func parseHeaders(headersString string) (map[string]string, error) {
 }
 
 // (ー_ー)!! I am ashamed of this, please do not talk to me about it
-// Parses the multivalue headers string we get from CloudWatch into a map[string][]string.
-func parseMultivalueHeaders(headersString string) (map[string][]string, error) {
+func parseMultivalueHeaders(headersString string) (map[string]string, error) {
 	headersString, err := trimHeadersString(headersString)
 	if err != nil {
 		return nil, err
 	}
-	headers := map[string][]string{}
-	for _, part := range strings.Split(headersString, "],") {
+	headers := map[string]string{}
+	for i, part := range strings.Split(headersString, "],") {
 		subparts := strings.SplitN(part, "=[", 2)
 		if len(subparts) != 2 {
 			return nil, fmt.Errorf("multivalue header had !=2 subparts when split by first '=[': %s", part)
 		}
-		headers[strings.Trim(subparts[0], " ")] = strings.Split(subparts[1], ", ")
+		if i == len(headers) && subparts[1][len(subparts[1])-1] == ']' {
+			subparts[1] = subparts[1][:len(subparts[1])-1]
+		}
+		headers[strings.Trim(subparts[0], " ")] = subparts[1]
 	}
 	return headers, nil
 }
